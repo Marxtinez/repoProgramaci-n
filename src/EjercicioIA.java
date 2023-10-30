fa599cbd7464a4f4bacae2e392f2cb8967a2cc7e
amm <adrmarmar8@alu.edu.gva.es>
2023-10-30 20:02:39 +0100

    Métodos creados

--- src/EjercicioIA.java
+++ src/EjercicioIA.java
@@ -1,2 +1,24 @@
+/*El juego consistirá, al menos de 5 rondas, donde el usuario y la IA, que tú vas a crear, escogen su jugada y se calcula el ganador.
+
+El programa tendrá al menos tres métodos:
+
+    Un método llamado jugadaIA que no tiene parámetros y devuelve la jugada del programa. Si optas por una solución con base aleatoria puedes consultar este artículo o bien el correspondiente a la documentación oficial.
+    Un método llamado calculaGanador que recibe dos parámetros, la jugada realizada por el usuario y la jugada realizada por la IA, y devuelve el resultado que puede ser: Gana el usuario, gana la IA o empate.
+    Un método llamado muestraEstadisticas, que recibe las jugadas realizadas en cada ronda y muestra:
+            El % de partidas ganadas por cada jugador y empates
+            El % ordenado de las jugadas seleccionadas (piedra, papel o tijera)*/
 public class EjercicioIA {
+    public static void main(String[] args){
+
+    }
+    public static String jugadaIA(){ //aleatorizarla
+
+    }
+    public static String calculaGanador(String jjugador, String jia){
+
+    }
+    public static String muestraEstadisticas(int ronda, String jjugador, String jia){
+
+    }
+
 }
